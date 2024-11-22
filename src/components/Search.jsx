968fcdd8c049ae2c8005f93018bb6bb08eeece16
RaulxAG <raul.arrgon@outlook.es>
2024-11-22 21:41:40 +0100

    added: styles & api util

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react'
+import './Search.css'
+
 export default function Search() {
+    const arrayLocations = ["Granada", "Jaén", "Almería"]
+    const [exampleLocation, setExampleLocation] = useState('')
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(prevIndex => (prevIndex + 1) % arrayLocations.length)
+        }, 5000)
+
+        return () => clearInterval(interval) // Limpia el intervalo al desmontar el componente
+    }, [])
+
+    useEffect(() => {
+        setExampleLocation(arrayLocations[index])
+    }, [index])
+
     return (
         <section>
-            <input type="search" name="search" id="search" placeholder="Madrid"/>
+            <input type="search" name="search" id="search" placeholder={exampleLocation} />
         </section>
     )
 }
---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+export const fetchWeatherData = async (location) => {
+    const apiKey = process.env.OPENWEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=es`;
+  
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("No se pudo obtener los datos del clima.");
+    }
+    return await response.json(); // Retorna los datos en formato JSON
+  };
+  
